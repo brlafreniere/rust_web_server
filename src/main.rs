0044b8952efb5d24418778903949a6c6530fa97f
Blaine Lafreniere <brlafreniere@gmail.com>
2020-12-15 13:06:50 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::Write;
+
+fn handle_client(mut stream: TcpStream) {
+    let data = b"lol hello\n";
+    stream.write_all(data).unwrap();
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+
+    Ok(())
+}
